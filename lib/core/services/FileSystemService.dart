8c72224a7a0d46f262627a0b6ee27e3e888130fb
Herob527 <szymon.wrzos@onet.pl>
2026-06-22 15:34:48 +0200

    Switch to parametrized getDirContent

--- lib/core/services/FileSystemService.dart
+++ lib/core/services/FileSystemService.dart
@@ -3,13 +3,9 @@ import 'dart:io';
 
 @injectable
 class FileSystemService {
-  Future<List<FileSystemEntity>> getDirContent() async {
-    final homeDir = Platform.environment['HOME'];
-    if (homeDir == null) {
-      throw Exception('HOME environment variable is not set');
-    }
+  Future<List<FileSystemEntity>> getDirContent(String searchDir) async {
     await Future.delayed(const Duration(seconds: 1));
-    final dir = Directory(homeDir);
+    final dir = Directory(searchDir);
     final items = await dir.list().toList();
     return items..sort(
       (a, b) => a.runtimeType.toString().compareTo(b.runtimeType.toString()),
