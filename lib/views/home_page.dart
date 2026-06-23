f7395d1242c59ce88270db52a1885c1981ddc97f
Herob527 <szymon.wrzos@onet.pl>
2026-06-23 21:03:06 +0200

    Replace icon

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
       children: [
         IconButton(
           onPressed: () => setState(() => _mode = .list),
-          icon: const Icon(Icons.arrow_back),
+          icon: const Icon(Icons.list),
         ),
 
         Expanded(
