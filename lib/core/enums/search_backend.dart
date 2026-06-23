bd078fa01da40c85bc056c6040ee5fae5b224cf9
Herob527 <szymon.wrzos@onet.pl>
2026-06-23 20:57:11 +0200

    Reformat + switch to expression

--- lib/core/enums/search_backend.dart
+++ lib/core/enums/search_backend.dart
@@ -3,20 +3,16 @@ enum SearchBackends {
     name: "ripgrep",
     description: "Search files containing given pattern",
   ),
-  fdfind(
-    name: "fdfind",
-    description: "Find files according to given pattern",
-  );
+  fdfind(name: "fdfind", description: "Find files according to given pattern");
 
   const SearchBackends({required this.name, required this.description});
 
   final String name;
   final String description;
 
-  String buildCommand({required String query, required String baseDir}) {
-    return switch (this) {
-      SearchBackends.ripgrep => "rg -l '$query' $baseDir",
-      SearchBackends.fdfind => "fd '$query' $baseDir",
-    };
-  }
+  String buildCommand({required String query, required String baseDir}) =>
+      switch (this) {
+        SearchBackends.ripgrep => "rg -l '$query' $baseDir",
+        SearchBackends.fdfind => "fd '$query' $baseDir",
+      };
 }
